222a02dee58348af4b722dad8bec5bca88a8ab56
MariaLuizaAdame33 <maluzinha.adame@gmail.com>
2024-03-12 10:00:41 -0300

    atualizand

--- ExemploJois.sql
+++ ExemploJois.sql
@@ -61,3 +61,72 @@ select frutas_id, cores_id from cores_frutas cf;
 insert into cores_frutas(cores_id,frutas_id)
 select c.id as cores_id, f.id as fruta_id
 from cores c cross join frutas f;
+
+
+create table clientes(
+   id int,
+   nome varchar(50),
+   primary key (id)
+);
+
+create table pedidos(
+   id int,
+   cliente_id int,
+   data_pedido date,
+   primary key(id),
+   constraint fk_pedidos_clientes
+   foreign key (cliente_id)
+   references clientes(id)
+);
+
+insert into clientes (id,nome) values
+(1,'João'),
+(2,'Maria'),
+(3,'Pedro'),
+(4,'Maitê'),
+(5,'Marcos'),
+(6,'Liz');
+
+insert into pedidos(id,cliente_id,data_pedido) values
+(101,1,'2024-03-01'),
+(102,1,'2024-03-05'),
+(103,2,'2024-03-02'),
+(104,3,'2024-03-03'),
+(105,3,'2024-03-07'),
+(106,4,'2024-03-06'),
+(107,2,'2024-03-08'),
+(108,5,'2024-03-09'),
+(109,5,'2024-03-10');
+
+insert into pedidos(id, data_pedido) values(10,'2024-03-11');
+
+select id, nome from clientes;
+select id, cliente_id,data_pedido from pedidos;
+
+/*
+ * INNER JOIN: Retorna registros quando há pelo menos uma correspondencia em
+ * abas as tabelas
+ */
+
+select c.id,c.nome,p.id,p.data_pedido,p.cliente_id
+from clientes c
+inner join pedidos p on c.id = p.cliente_id;
+
+/*
+ * neste exemplo estamos selecionando todas as colunas das tabelas (clientes e pedidos)
+ * onde há uma correspondencia entre o id na tabela clientes e o cliente_id na tabela 
+ * pedidos.
+ */
+
+/*
+ * LEFT JOIN:Retorna todos os registro da tabela A (primeira tabela selecionada)
+ * e os registros da tabela B(segunda tabela).
+ */
+select c.id,c.nome,p.id,p.data_pedido,p.cliente_id
+from clientes c
+left join pedidos p on c.id = p.cliente_id;
+
+/*
+ * Aqui estamos selecionando todos os registros da tabela clientes e os registros da tabela pedido.
+ */
+
